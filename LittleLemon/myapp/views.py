967d6d8aac265432736a9238c8a655cd22b55414
Sarah Stenfert <sarah.stenfert@gmail.com>
2025-11-19 14:22:31 +1300

    Started creating a blog for the Little Lemon restaurant. Created a model form and a simple function-based view, form_view. Started creating a blog.html file.

--- LittleLemon/myapp/views.py
+++ LittleLemon/myapp/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from myapp.forms import CommentForm
+from .models import UserComments
+from django.http import JsonResponse
 
-# Create your views here.
+def form_view(request):
+    form = CommentForm()
+    
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            # cleaned_data is an attribute of the form object
+            # When you call form.is_valid(), Django automatically populates the cleaned_data dictionary attribute with the validated and cleaned form data
+            cd = form.cleaned_data
+            uc = UserComments(
+                first_name = cd['first_name'],
+                last_name = cd['last_name'],
+                comment = cd['comment'],
+            )
+            # Save the data to the database
+            uc.save()
+        
+        return render(request, 'blog.html', {'form': form})
