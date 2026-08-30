f254d9b1be3f7cc3968c4c031d29e744d6ac7d5e
Sarah Stenfert <sarah.stenfert@gmail.com>
2026-08-30 17:08:33 +1200

    Added the test user credentials for the mysql database. Not used in production.

--- C7M5L1-6 Lab Initial/littlelemon/settings.py
+++ C7M5L1-6 Lab Initial/littlelemon/settings.py
@@ -80,10 +80,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'reservations',
-        'HOST' : '127.0.0.1',
-        'PORT' : '3306',
-        'USER' : 'root',
-        'PASSWORD' : 'root@123',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'USER': 'admindjango',
+        'PASSWORD': 'employee@123!',
     }
 }
 
